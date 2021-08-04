77a8e004820c0fdc17876f4c3a0bab9087a1e4b1
Abhishekmohan7171 <abhishekmohan7171@gmail.com>
2021-08-05 01:32:08 +0530

    final commit

--- speechify.py
+++ speechify.py
@@ -1,4 +1,21 @@
 import pyttsx3
+import PyPDF2
+book = open("trial.pdf", 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+
+# for a certain page
+# speaker = pyttsx3.init()
+# page = pdfReader.getPage(1)
+# text = page.extractText()
+# speaker.say(text)
+# speaker.runAndWait()
+
+#for a range of pages
 speaker = pyttsx3.init()
-speaker.say('hey iam abhishek')
-speaker.runAndWait()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
